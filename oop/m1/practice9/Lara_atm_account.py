f2bb93e04533938ce182e63c8a8d9c4ab507fb27
Elizabeth Maude M. Lara <elira2707@gmail.com>
2026-09-02 15:35:17 +0000

    Activiy 1 and 2 completed.

---
+++ oop/m1/practice9/Lara_atm_account.py
@@ -0,0 +1,35 @@
+class Account:
+    def __init__(self, name, starting_balance):
+        self.account_name = name
+        self._balance = starting_balance
+    
+    def check_balance(self):
+        print(f"Current Balance: ₱{self._balance:.2f}")
+    
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+            return True
+        else:
+            return False
+    
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self._balance:
+            self._balance -= amount
+            return True
+        else:
+            return False
+
+""" 
+######### Learning Signature ######### 
+Programmed by: Elizabeth Maude M. Lara
+Date Submitted: September 02, 2026
+ 
+Program Description: This program is a ATM command line interface where account holders can view their current balance, add money to their account, and take money out.
+Reflection: I learned to write a class to hold account data and methods for deposits and withdrawals, which made the code easier to organize and expand.
+ 
+AI Usage
+[/] No AI Assistance - Completed independently without AI.
+[ ] AI as Support Tool - Used AI for explanations, syntax, or minor corrections.
+[ ] AI as Collaborative Partner - Used AI to design, structure, or co-create significant code.
+"""
---
+++ oop/m1/practice9/Lara_atm_balance.py
@@ -0,0 +1,21 @@
+from Lara_atm_account import Account
+
+def check_balance(account):
+    return account.check_balance()
+
+account = Account("Ele", 10000.00)
+account.check_balance()
+
+""" 
+######### Learning Signature ######### 
+Programmed by: Elizabeth Maude M. Lara
+Date Submitted: September 02, 2026
+ 
+Program Description: This program is a ATM command line interface where users can only check their account balance.
+Reflection: I learned to write a class to store balance data and a method to display, it helped me understand how objects can represent real world account features.
+
+AI Usage
+[/] No AI Assistance - Completed independently without AI.
+[ ] AI as Support Tool - Used AI for explanations, syntax, or minor corrections.
+[ ] AI as Collaborative Partner - Used AI to design, structure, or co-create significant code.
+"""
